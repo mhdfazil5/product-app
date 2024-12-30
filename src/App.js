0600f19b77a4ccc665f25a320d24db1176236a89
mhdfazil5 <fasil10hamsa@gmail.com>
2024-12-30 21:41:10 +0530

    product

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import FirstComponent from './components/FirstComponent';
+import SecondComponent from './components/SecondComponent';
+import ShoppingCart from './components/ShoppingCart';
+
+function App() {
+  return (
+    <div>
+      <FirstComponent />
+      <SecondComponent />
+      <ShoppingCart />
+
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+
+const ShoppingCart = () => {
+  return (
+    <div>
+
+            <div className="container">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://m.media-amazon.com/images/I/61smvoZKNRL.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Apple ipad air(5th gen)</h5>
+    <p class="card-text">₹63,199.00</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+                                 </div>
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://m.media-amazon.com/images/I/61PBLEFPoKL._AC_UF894,1000_QL80_.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">iphone 16 pro max </h5>
+    <p class="card-text">₹141,900.00 </p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+                                 </div>
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://m.media-amazon.com/images/I/81kq5wkaqAL._AC_UF1000,1000_QL80_.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Apple watch series 10</h5>
+    <p class="card-text">₹59,900.00</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+                                 </div>
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://cdsassets.apple.com/live/7WUAS350/images/tech-specs/apple_vision_pro_hero.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Apple vision pro</h5>
+    <p class="card-text">3,99,999</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+                                 </div>
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MUYG3_AV3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1701189162296" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Apple airpods 4</h5>
+    <p class="card-text">₹9,900.00 </p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+                                 </div>
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airtag-double-select-202104_FMT_WHH?wid=2000&hei=2000&fmt=jpeg&qlt=90&.v=1617761672000" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Apple airtag</h5>
+    <p class="card-text">
+    ₹2,999.00</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+                                 </div>
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://m.media-amazon.com/images/I/718+IbM3rIL._AC_UF894,1000_QL80_.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Apple mac mini</h5>
+    <p class="card-text">₹59,900.00</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+                                 </div>
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/apple-tv-4k-hero-select-202210_FMT_WHH?wid=320&hei=300&fmt=jpeg&qlt=95&.v=1664896361164" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Apple TV</h5>
+    <p class="card-text">₹14,900.00 </p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+                                 </div>
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://www.apple.com/newsroom/images/product/imac/standard/Apple_imac-magickeyboardnum-magicmouse2-macos-wallpaper_08042020.jpg.news_app_ed.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Apple imac</h5>
+    <p class="card-text">₹108,515.00</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+                                 </div>
+                                 <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                 <div class="card" style="width: 18rem;">
+  <img src="https://www.apple.com/newsroom/images/product/iphone/geo/Apple-iPhone-14-Pro-iPhone-14-Pro-Max-deep-purple-220907-geo_inline.jpg.large.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">I phone 14 pro max</h5>
+    <p class="card-text">208,679.00</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+                                 </div>
+
+                    </div>
+                </div>
+            </div>
+
+    </div>
+  )
+}
+
+export default ShoppingCart
