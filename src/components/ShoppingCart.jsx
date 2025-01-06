fe253368d8454f9879b95e57b7d6edf1a7205533
mhdfazil5 <fasil10hamsa@gmail.com>
2025-01-06 22:11:34 +0530

    renewed

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -10,20 +10,23 @@ const ShoppingCart = () => {
 
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                 <div class="card" style="width: 18rem;">
+                                 <div class="card" >
   <img src="https://m.media-amazon.com/images/I/61smvoZKNRL.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Apple ipad air(5th gen)</h5>
     <p class="card-text">₹63,199.00</p>
     <a href="#" class="btn btn-primary">Buy Now</a>
+    </img>
+
+    
   </div>
 </div>
 
                                  </div>
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                 <div class="card" style="width: 18rem;">
-  <img src="https://m.media-amazon.com/images/I/61PBLEFPoKL._AC_UF894,1000_QL80_.jpg" class="card-img-top" alt="...">
+                                 <div class="card" >
+  <img src="https://m.media-amazon.com/images/I/61PBLEFPoKL._AC_UF894,1000_QL80_.jpg" class="card-img-top" alt="..."></img>
   <div class="card-body">
     <h5 class="card-title">iphone 16 pro max </h5>
     <p class="card-text">₹141,900.00 </p>
@@ -34,8 +37,8 @@ const ShoppingCart = () => {
                                  </div>
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                 <div class="card" style="width: 18rem;">
-  <img src="https://m.media-amazon.com/images/I/81kq5wkaqAL._AC_UF1000,1000_QL80_.jpg" class="card-img-top" alt="...">
+                                 <div class="card" >
+  <img src="https://m.media-amazon.com/images/I/81kq5wkaqAL._AC_UF1000,1000_QL80_.jpg" class="card-img-top" alt="..."></img>
   <div class="card-body">
     <h5 class="card-title">Apple watch series 10</h5>
     <p class="card-text">₹59,900.00</p>
@@ -46,20 +49,20 @@ const ShoppingCart = () => {
                                  </div>
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                 <div class="card" style="width: 18rem;">
-  <img src="https://cdsassets.apple.com/live/7WUAS350/images/tech-specs/apple_vision_pro_hero.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Apple vision pro</h5>
-    <p class="card-text">3,99,999</p>
-    <a href="#" class="btn btn-primary">Buy Now</a>
-  </div>
-</div>
+                                 <div class="card" >
+                                   <img src="https://cdsassets.apple.com/live/7WUAS350/images/tech-specs/apple_vision_pro_hero.png" class="card-img-top" alt="..."></img>
+                                   <div class="card-body">
+                                     <h5 class="card-title">Apple vision pro</h5>
+                                        <p class="card-text">3,99,999</p>
+                                               <a href="#" class="btn btn-primary">Buy Now</a>
+                                            </div>
+                                         </div>
 
                                  </div>
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
 
-                                 <div class="card" style="width: 18rem;">
+                                 <div class="card" >
   <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MUYG3_AV3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1701189162296" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Apple airpods 4</h5>
@@ -71,7 +74,7 @@ const ShoppingCart = () => {
                                  </div>
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                 <div class="card" style="width: 18rem;">
+                                 <div class="card" >
   <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airtag-double-select-202104_FMT_WHH?wid=2000&hei=2000&fmt=jpeg&qlt=90&.v=1617761672000" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Apple airtag</h5>
@@ -84,8 +87,8 @@ const ShoppingCart = () => {
                                  </div>
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                 <div class="card" style="width: 18rem;">
-  <img src="https://m.media-amazon.com/images/I/718+IbM3rIL._AC_UF894,1000_QL80_.jpg" class="card-img-top" alt="...">
+                                 <div class="card" >
+  <img src="https://m.media-amazon.com/images/I/718+IbM3rIL._AC_UF894,1000_QL80_.jpg" class="card-img-top" alt="..."></img>
   <div class="card-body">
     <h5 class="card-title">Apple mac mini</h5>
     <p class="card-text">₹59,900.00</p>
@@ -96,7 +99,7 @@ const ShoppingCart = () => {
                                  </div>
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                 <div class="card" style="width: 18rem;">
+                                 <div class="card" >
   <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/apple-tv-4k-hero-select-202210_FMT_WHH?wid=320&hei=300&fmt=jpeg&qlt=95&.v=1664896361164" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Apple TV</h5>
@@ -109,7 +112,7 @@ const ShoppingCart = () => {
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
 
-                                 <div class="card" style="width: 18rem;">
+                                 <div class="card" >
   <img src="https://www.apple.com/newsroom/images/product/imac/standard/Apple_imac-magickeyboardnum-magicmouse2-macos-wallpaper_08042020.jpg.news_app_ed.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Apple imac</h5>
@@ -120,7 +123,7 @@ const ShoppingCart = () => {
                                  </div>
                                  <div className="row g-3 col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                 <div class="card" style="width: 18rem;">
+                                 <div class="card" >
   <img src="https://www.apple.com/newsroom/images/product/iphone/geo/Apple-iPhone-14-Pro-iPhone-14-Pro-Max-deep-purple-220907-geo_inline.jpg.large.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">I phone 14 pro max</h5>
